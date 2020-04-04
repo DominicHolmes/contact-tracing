0168437243e342f89d27b607219bcbb47dbcf99e
dominic <hello@dom.voyage>
2020-04-04 14:51:00 -0700

    initial commit

---
+++ contact-tracing/contact-tracing/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  contact-tracing
+//
+//  Created by Dominic Holmes on 4/4/20.
+//  Copyright © 2020 Dominic Holmes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ contact-tracing/contact-tracing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contact-tracing
+//
+//  Created by Dominic Holmes on 4/4/20.
+//  Copyright © 2020 Dominic Holmes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
