da12d2a4f0e75d0996fcd382a1a1d822b3098264
Eric Wang <ezwang1@gmail.com>
2020-04-05 11:52:39 -0400

    theoretically implement bluetooth foreground service handling

--- android/app/src/main/java/com/example/contact_tracing/MainActivity.kt
+++ android/app/src/main/java/com/example/contact_tracing/MainActivity.kt
@@ -1,135 +1,120 @@
 package com.example.contact_tracing
 
-import android.app.PendingIntent
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.le.*
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.nio.ByteBuffer
-import java.util.*
+import org.altbeacon.beacon.*
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver
+import android.Manifest
+import android.app.*
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 
-class BeaconReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        var callbackType = intent?.getIntExtra(BluetoothLeScanner.EXTRA_CALLBACK_TYPE, -1)
-        if (callbackType != -1) {
-            Log.i("bluetooth", "background scan callback: $callbackType")
-        }
-    }
-}
+class MainActivity : AppCompatActivity(), BeaconConsumer {
+    var beaconManager: BeaconManager? = null
+    var backgroundPowerSaver: BackgroundPowerSaver? = null
+    var transmitter : BeaconTransmitter? = null
 
-const val APPLE_MANUFACTURER_ID : Int = 76
-
-class MainActivity : AppCompatActivity() {
-    var adapter : BluetoothAdapter? = null
-    var scanIntent : PendingIntent? = null
-    var advertiseCallback : AdvertiseCallback? = null
-
-    private fun getEncodedUUID() : ByteBuffer {
-        var uuid = UUID.fromString("9eb121f4-1bc2-4da6-862f-60dcedabcf38");
-        var encodedUUID = ByteBuffer.allocate(16)
-        encodedUUID.putLong(uuid.mostSignificantBits)
-        encodedUUID.putLong(uuid.leastSignificantBits)
-        return encodedUUID
-    }
-
-    private fun getScanFilter() : List<ScanFilter> {
-        var builder = ScanFilter.Builder()
-        var manufacturerData = ByteBuffer.allocate(23)
-        var manufacturerDataMask = ByteBuffer.allocate(24)
-        manufacturerData.put(0,(0x02).toByte())
-        manufacturerData.put(1, (0x15).toByte())
-        var encodedUUID = getEncodedUUID()
-        for (i in 2..16) {
-            manufacturerData.put(i, encodedUUID[i - 2])
-        }
-        for (i in 2..16) {
-            manufacturerData.put((0x01).toByte())
-        }
-        builder.setManufacturerData(APPLE_MANUFACTURER_ID, manufacturerData.array(), manufacturerDataMask.array())
-        return listOf(builder.build())
+    private fun startTransmitter() {
+        Log.i("bluetooth", "starting transmitter")
+        var beaconBuilder = Beacon.Builder()
+        beaconBuilder.setId1("31049370-03e0-459f-b832-da0666df02f8")
+        beaconBuilder.setId2("0")
+        beaconBuilder.setId3("0")
+        beaconBuilder.setManufacturer(0x004c)
+        beaconBuilder.setTxPower(-59)
+        beaconBuilder.setDataFields(listOf(0L))
+        var beacon = beaconBuilder.build()
+        var parser = BeaconParser()
+        parser.setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        transmitter = BeaconTransmitter(applicationContext, parser)
+        transmitter?.startAdvertising(beacon)
+        Log.i("bluetooth", "transmitter started")
     }
 
-    private fun startScanner() {
-        var settings = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
-        var intent = Intent(applicationContext, BeaconReceiver::class.java)
-        scanIntent = PendingIntent.getBroadcast(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-        adapter?.bluetoothLeScanner?.startScan(getScanFilter(), settings, scanIntent!!)
-    }
-
-    private fun stopScanner() {
-        scanIntent.let {
-            adapter?.bluetoothLeScanner?.stopScan(it)
-        }
-        scanIntent = null
-    }
-
-    private fun startAdvertiser() {
-        var advSettings = AdvertiseSettings.Builder()
-        advSettings.setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
-        advSettings.setConnectable(false)
-        advSettings.setTimeout(0)
-        advSettings.setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
-
-        var dataBuilder = AdvertiseData.Builder()
-        var manufacturerData = ByteBuffer.allocate(24)
-        manufacturerData.put(0, (0x02).toByte())
-        manufacturerData.put(1, (0x15).toByte())
-        var encodedUUID = getEncodedUUID()
-        for (i in 2..16) {
-            manufacturerData.put(i, encodedUUID[i - 2])
-        }
-        // major
-        manufacturerData.put(18, (0x00).toByte())
-        manufacturerData.put(19, (0x09).toByte())
-        // minor
-        manufacturerData.put(20, (0x00).toByte())
-        manufacturerData.put(21, (0x06).toByte())
-        // tx power
-        manufacturerData.put(22, (0xb5).toByte())
-        dataBuilder.addManufacturerData(APPLE_MANUFACTURER_ID, manufacturerData.array())
-        var advData = dataBuilder.build()
-
-        advertiseCallback = object: AdvertiseCallback() {
-            override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
-                Log.i("bluetooth", "advertisement started")
-            }
-
-            override fun onStartFailure(errorCode: Int) {
-                Log.w("bluetooth", "advertisement failed $errorCode")
-            }
-        }
-
-        adapter?.bluetoothLeAdvertiser?.startAdvertising(advSettings.build(), advData, advertiseCallback!!)
-    }
-
-    private fun stopAdvertiser() {
-        advertiseCallback.let {
-            adapter?.bluetoothLeAdvertiser?.stopAdvertising(it)
-        }
-        advertiseCallback = null
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = BluetoothAdapter.getDefaultAdapter()
-
-        if (adapter?.isEnabled != true) {
-            Log.i("bluetooth", "bluetooth is not enabled");
+        // prompt user for location, this is necessary in order for permissions to work
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            }
+            else {
+                initBeacons()
+            }
         }
         else {
-            Log.i("bluetooth", "bluetooth is enabled");
+            initBeacons()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initBeacons()
+            }
+        }
+    }
+
+    private fun initBeacons() {
+        // init beacon manager
+        beaconManager = BeaconManager.getInstanceForApplication(this)
+
+        // foreground service requires channel in newer android
+        var channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel("contact-tracing", "Stop the Spread")
+        } else {
+            ""
         }
 
-        // register receiver
-        startScanner()
+        // setup beacon manager to run as a foreground service
+        var builder = Notification.Builder(this.applicationContext, channelId)
+        beaconManager?.enableForegroundServiceScanning(builder.build(), 456)
+        beaconManager?.setEnableScheduledScanJobs(false)
 
-        // register sender
-        startAdvertiser()
+        // enable ibeacons
+        beaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+        beaconManager?.bind(this)
+
+        backgroundPowerSaver = BackgroundPowerSaver(this)
+
+        // check transmitter service status
+        var transmitterResult = BeaconTransmitter.checkTransmissionSupported(this)
+        if (transmitterResult != BeaconTransmitter.SUPPORTED) {
+            Log.w("bluetooth", "does not support transmission $transmitterResult")
+        }
+    }
+
+    override fun onBeaconServiceConnect() {
+        beaconManager?.removeAllMonitorNotifiers();
+        beaconManager?.addRangeNotifier { beacons, _ ->
+            Log.i(
+                "bluetooth",
+                "ranged ${beacons?.size} beacons"
+            )
+        }
+        beaconManager?.startMonitoringBeaconsInRegion(Region("contact-tracing", null, null, null))
+        Log.i("bluetooth", "listener started")
+
+        startTransmitter()
     }
 }
