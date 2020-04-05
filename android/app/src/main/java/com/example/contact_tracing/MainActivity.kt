9c565dfb9ca593db536f4958d79982459fe46b11
Eric Wang <ezwang1@gmail.com>
2020-04-05 13:59:50 -0400

    add toggle

--- android/app/src/main/java/com/example/contact_tracing/MainActivity.kt
+++ android/app/src/main/java/com/example/contact_tracing/MainActivity.kt
@@ -12,13 +12,19 @@ import android.Manifest
 import android.app.*
 import android.bluetooth.le.AdvertiseCallback
 import android.bluetooth.le.AdvertiseSettings
+import android.graphics.Color
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
-const val APP_UUID : String = "31049370-03e0-459f-b832-da0666df02f8"
+const val APP_UUID: String = "31049370-03e0-459f-b832-da0666df02f8"
 
 class MainActivity : AppCompatActivity(), BeaconConsumer {
     var beaconManager: BeaconManager? = null
     var backgroundPowerSaver: BackgroundPowerSaver? = null
-    var transmitter : BeaconTransmitter? = null
+    var transmitter: BeaconTransmitter? = null
 
     private fun startTransmitter() {
         Log.i("bluetooth", "starting transmitter")
@@ -33,7 +39,7 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
         var parser = BeaconParser()
         parser.setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
         transmitter = BeaconTransmitter(applicationContext, parser)
-        transmitter?.startAdvertising(beacon, object: AdvertiseCallback() {
+        transmitter?.startAdvertising(beacon, object : AdvertiseCallback() {
             override fun onStartFailure(errorCode: Int) {
                 Log.w("bluetooth", "transmitter failed with error code $errorCode")
             }
@@ -52,22 +58,57 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
         return channelId
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+    private fun startInitFlow() {
         // prompt user for location, this is necessary in order for permissions to work
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
-            }
-            else {
+            } else {
                 initBeacons()
             }
-        }
-        else {
+        } else {
             initBeacons()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // setup toggle button
+        var btnToggle = findViewById<Switch>(R.id.btnToggle)
+        var lblTrackingTitle = findViewById<TextView>(R.id.lblTrackingTitle)
+        var lblTrackingSubtitle = findViewById<TextView>(R.id.lblTrackingSub)
+        var layoutTracking = findViewById<ConstraintLayout>(R.id.layoutTracking)
+        btnToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (!isChecked) {
+                beaconManager?.removeAllMonitorNotifiers()
+                transmitter?.stopAdvertising()
+                Toast.makeText(this, "Service disabled!", Toast.LENGTH_SHORT).show()
+            } else {
+                if (beaconManager == null) {
+                    startInitFlow()
+                }
+                else {
+                    onBeaconServiceConnect()
+                    Toast.makeText(this, "Service enabled!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            if (isChecked) {
+                lblTrackingTitle.text = "Tracking: ON"
+                lblTrackingSubtitle.text = "Back home? Turn me off to save battery."
+                layoutTracking.setBackgroundColor(Color.parseColor("#F3755E"))
+
+            }
+            else {
+                lblTrackingTitle.text = "Tracking: OFF"
+                lblTrackingSubtitle.text = "Going outside? Turn me on!"
+                layoutTracking.setBackgroundColor(Color.parseColor("#E5E5E5"))
+            }
+        }
+
+        btnToggle.isChecked = true
     }
 
     override fun onRequestPermissionsResult(
